bc9ae71a937386a99c8b8b055fd1ad95d5658f12
FloBaBa33 <florian_baumbach@hotmail.de>
2023-01-22 18:45:37 +0100

    added message reactions for starlight

--- bot.js
+++ bot.js
@@ -99,7 +99,7 @@ client.on ( 'messageCreate', async ( message ) => {
             try {
                 await message.react ( emote )
             } catch ( err ) {
-                await logError ( `${err}\nEmote: ${emote}` )
+                await logError ( `${ err }\nEmote: ${ emote }` )
             }
         }
     } else if ( await checkForPictures ( message ) && nsfw.channel.includes( channel.id )) {
@@ -107,7 +107,7 @@ client.on ( 'messageCreate', async ( message ) => {
             try {
                 await message.react ( emote )
             } catch ( err ) {
-                await logError ( `${err}\nEmote: ${emote}` )
+                await logError ( `${ err }\nEmote: ${ emote }` )
             }
         }
     } else if ( await checkForPictures ( message ) && smash.channel.includes ( channel.id )) {
@@ -115,7 +115,43 @@ client.on ( 'messageCreate', async ( message ) => {
             try {
                 await message.react ( emote )
             } catch ( err ) {
-                await logError ( `${err}\nEmote: ${emote}` )
+                await logError ( `${ err }\nEmote: ${ emote }` )
+            }
+        }
+    } else if ( await checkForPictures ( message ) && message.id === '1051969089486209044') { //starlight selfie
+        const emotes = [ '<a:Aladdin:1063871623779852358>', '<a:Aristocats:1063871582994436206>', '<a:olaf:1064970403589660742>', '<a:CheshireCat:1064970392663507074> ' ]
+        for ( const emote of emotes ) {
+            try {
+                await message.react ( emote )
+            } catch ( err ) {
+                await logError ( `${ err }\nEmote: ${ emote }`)
+            }
+        }
+    } else if ( await checkForPictures ( message ) && message.id === '1051969109149098005') { //starlight male nude
+        const emotes = [ '<a:Donaldcountingmoney:1064970391283581008>', '<a:ToyStory:1063871592746197062>', '<a:Gaston:1063871577948700724>', '<a:Hook:1063871570638024784> ' ]
+        for ( const emote of emotes ) {
+            try {
+                await message.react ( emote )
+            } catch ( err ) {
+                await logError ( `${ err }\nEmote: ${ emote }`)
+            }
+        }
+    } else if ( await checkForPictures ( message ) && message.id === '1051969107865649173') { // starlight female nude
+        const emotes = [ '<a:Donaldcountingmoney:1064970391283581008>', '<a:Tink:1063871616691486760>', '<a:Stitch2:1063872443455918260>', '<:winkpuppy:1064970396652286012> ' ]
+        for ( const emote of emotes ) {
+            try {
+                await message.react ( emote )
+            } catch ( err ) {
+                await logError ( `${ err }\nEmote: ${ emote }`)
+            }
+        }
+    } else if ( await checkForPictures ( message ) && message.id === '1051969110449344522') { // starlight lgbtq nude
+        const emotes = [ ':rainbow_flag: ', '<a:DisneyD:1063870141798035516>', '<a:Donaldcountingmoney:1064970391283581008>', '<a:MonstersInc:1063871600937685072>' ]
+        for ( const emote of emotes ) {
+            try {
+                await message.react ( emote )
+            } catch ( err ) {
+                await logError ( `${ err }\nEmote: ${ emote }`)
             }
         }
     }
